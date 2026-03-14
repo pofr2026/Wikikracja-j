6ec0d9c5fa7bacd58e5cd1564b814ba21d0b5104
Robert Fialek <robert.fialek@gmail.com>
2026-03-14 08:59:29 +0100

    Use get_or_create for voting chat rooms to prevent race condition duplicates
    - Replace Room.objects.create() with get_or_create() in vote command
    - Only set allowed users if room was just created or has incorrect user count
    - Add comment explaining race condition handling

--- glosowania/management/commands/vote.py
+++ glosowania/management/commands/vote.py
@@ -216,9 +216,13 @@ class Command(BaseCommand):
                 if existing_room is not None:
                     existing_room.title = title
                     existing_room.save()
-                # if not - create new room
+                # if not - create new room (use get_or_create to handle race conditions)
                 else:
-                    r = Room.objects.create(title=title, public=False)
-                    r.allowed.set((i, j,))
+                    r, created = Room.objects.get_or_create(
+                        title=title,
+                        defaults={'public': False}
+                    )
+                    if created or r.allowed.count() != 2:
+                        r.allowed.set((i, j,))
 
         log.info(f'vote.py counted all votes')
